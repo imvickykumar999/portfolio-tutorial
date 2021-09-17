f82de7605b1301abfb8c8ca495b2ae61a40284e0
imvickykumar999 <imvickykumar999@gmail.com>
2021-09-17 12:37:27 +0530

    href

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('portfolio.html')
+    return render_template('portfolio.html', name = 'Vicky Kumar')
 
 if __name__ == '__main__':
     app.run(debug=True)
