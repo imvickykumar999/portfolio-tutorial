f47088b5dd3982e2ec2fe770d6b362ef6a46dbf9
imvickykumar999 <imvickykumar999@gmail.com>
2021-09-18 17:15:29 +0530

    404

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return ( render_template('404.html'), 404 )
 
 if __name__ == '__main__':
     app.run(debug=True)
